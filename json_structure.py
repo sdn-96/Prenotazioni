d3854359c58108ffdac59f73edb5450b06df68de
sdn-96 <71753364+sdn-96@users.noreply.github.com>
2025-04-14 17:23:37 +0200

    Create json_structure.py
    define how the json have to be structured for both base and changes files

---
+++ json_structure.py
@@ -0,0 +1,72 @@
+'''
+json structure for base retrived data
+{
+  "type": "base"
+  "columns": [
+    "Appartamento",
+    "Nome",
+    "Nazione",
+    "Check in",
+    "Check out",
+    "Notti",
+    "Totale pernottamento",
+    "Tot proprietario",
+    "Netto proprietario",
+    ""
+  ],
+  "rows": [
+    [
+      "L. BRIENNO",
+      "3540/2024",
+      "",
+      "06/06/2024",
+      "13/06/2024",
+      "7",
+      "€\t\t\t\t\t\t\t\t\t\t611,56",
+      "€395,96",
+      "€395,96",
+      ""
+    ],
+    [
+    ...
+    ]   
+  ]
+}
+'''
+'''
+json structure for base changes data
+{
+  "type": "change"
+  "columns": [
+    "Type": "added"/"removed"/"changed"
+    "Appartamento",
+    "Nome",
+    "Nazione",
+    "Check in",
+    "Check out",
+    "Notti",
+    "Totale pernottamento",
+    "Tot proprietario",
+    "Netto proprietario",
+    ""
+  ],
+  "rows": [
+    [
+      "added"/"removed"/"changed"
+      "L. BRIENNO",
+      "3540/2024",
+      "",
+      "06/06/2024",
+      "13/06/2024",
+      "7",
+      "€\t\t\t\t\t\t\t\t\t\t611,56",
+      "€395,96",
+      "€395,96",
+      ""
+    ],
+    [
+    ...
+    ]   
+  ]
+}
+'''
